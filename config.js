021aa168fb06d4bb72f80838f65cf588ca66e937
Yves <yalain9@gmail.com>
2018-11-05 20:54:29 -0600

    db source update

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    // 'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://yves:password11@ds139352.mlab.com:39352/ristorante'
 };
